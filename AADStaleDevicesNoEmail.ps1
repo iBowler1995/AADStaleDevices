7c1bf80be284c9b7caa9132d04e95d2c9250c1e6
iBowler1995 <93746898+iBowler1995@users.noreply.github.com>
2021-11-15 16:22:25 -0500

    Update AADStaleDevicesNoEmail.ps1
    Corrected logic used in DisableDevices switch so it pulls ObjectId automatically.

--- AADStaleDevicesNoEmail.ps1
+++ AADStaleDevicesNoEmail.ps1
@@ -1,9 +1,9 @@
-﻿<#	
+<#	
 	NOTES
 	===========================================================================
 	 Script Name: AADStaleDevices
 	 Created on:   	11/4/2021
-	 Created by:   	iBowler1995
+	 Created by:   	Levi Masse
 	 Filename: AADStaleDevices.ps1
 	===========================================================================
 	.DESCRIPTION
@@ -51,11 +51,9 @@ $Date = ("{0:s}" -f (Get-Date)).Split("T")[0]
 $Days = [datetime](get-date).AddDays(- $Threshold)
 #This should be the account with appropriate permission to execute this script
 $UPN = ""
-If ($SPWDExists)
-{
-	$SPWD = Get-Content ".\SCred.txt" | ConvertTo-SecureString
-	$ScriptCredential = New-Object -TypeName System.Management.Automation.PSCredential($UPN, $SPWD)
-}
+$SPWD = Get-Content ".\SCred.txt" | ConvertTo-SecureString      
+$ScriptCredential = New-Object -TypeName System.Management.Automation.PSCredential($UPN, $SPWD)
+
 #Connects to AzureAD, with saved creds if the flag is called
 If ($UseCreds)
 {
@@ -69,30 +67,30 @@ else
 If ($Verify)
 {
 	$vPath = ".\Exports\Stale Azure Devices_" + $Date + ".xlsx"
-	$GetStaleDevices = Get-AzureADDevice -All:$true | Where { $_.ApproximateLastLogonTimeStamp -le $Days } | select-object -Property AccountEnabled, DeviceId, DeviceOSVersion, DisplayName, ApproximateLastLogonTimestamp
+	$GetStaleDevices = Get-AzureADDevice -All:$true | Where { $_.ApproximateLastLogonTimeStamp -le $Days } | select-object -Property AccountEnabled, ObjectId, DeviceId, DeviceOSVersion, DisplayName, ApproximateLastLogonTimestamp
 	$GetStaleDevices | Export-Excel -workSheetName "Stale Devices" -path $vPath -ClearSheet -TableName "Stale AAD Devices" -AutoSize
 }
 elseif ($VerifyDisabled)
 {
 	$vdPath = ".\Exports\Stale Disabled Azure Devices_" + $Date + ".xlsx"
-	$GetDisabledStaleDevices = Get-AzureADDevice -All:$true | where { ($_.ApproximateLastLogonTimeStamp -le $Days) -and ($_.AccountEnabled -eq $false) } | select-object -Property AccountEnabled, DeviceId, DeviceOSVersion, DisplayName, ApproximateLastLogonTimestamp
+	$GetDisabledStaleDevices = Get-AzureADDevice -All:$true | where { ($_.ApproximateLastLogonTimeStamp -le $Days) -and ($_.AccountEnabled -eq $false) } | select-object -Property AccountEnabled, ObjectId, DeviceId, DeviceOSVersion, DisplayName, ApproximateLastLogonTimestamp
 	$GetDisabledStaleDevices | Export-Excel -workSheetName "Stale Disabled Devices" -path $vdPath -ClearSheet -TableName "Stale Disabled AAD Devices" -AutoSize
 }
 elseif ($DisableDevices)
 {
 	$ddPath = ".\Exports\Azure Devices Disabled_" + $Date + ".xlsx"
-	$DisableAADDevice = Get-AzureADDevice -All:$true | where { ($_.ApproximateLastLogonTimeStamp -le $Days) } | select-object -Property AccountEnabled, DeviceId, DeviceOSVersion, DisplayName, ApproximateLastLogonTimestamp
+	$DisableAADDevice = Get-AzureADDevice -All:$true | where { ($_.ApproximateLastLogonTimeStamp -le $Days) } | select-object -Property AccountEnabled, ObjectId, DeviceId, DeviceOSVersion, DisplayName, ApproximateLastLogonTimestamp
 	$DisableAADDevice | Export-Excel -workSheetName "Devices Disabled" -path $ddPath -ClearSheet -TableName "AAD Devices Disabled" -AutoSize
 	
 	foreach ($StaleDevice in $DisableAADDevice)
 	{
-		Set-AzureADDevice -AccountEnabled $false
+		Set-AzureADDevice -ObjectId $StaleDevice.ObjectID -AccountEnabled $false
 	}
 }
 elseif ($RemoveDevices)
 {
 	$rdPath = ".\Exports\Azure Devices Removed_" + $Date + ".xlsx"
-	$RemoveAADDeviceList = Get-AzureADDevice -All:$true | where { ($_.ApproximateLastLogonTimeStamp -le $Days) -and ($_.AccountEnabled -eq $false) } | select-object -Property AccountEnabled, DeviceId, DeviceOSVersion, DisplayName, ApproximateLastLogonTimestamp
+	$RemoveAADDeviceList = Get-AzureADDevice -All:$true | where { ($_.ApproximateLastLogonTimeStamp -le $Days) -and ($_.AccountEnabled -eq $false) } | select-object -Property AccountEnabled, ObjectId, DeviceId, DeviceOSVersion, DisplayName, ApproximateLastLogonTimestamp
 	$RemoveAADDeviceList | Export-Excel -workSheetName "Stale Devices Removed" -Path $rdPath -ClearSheet -TableName "AAD Devices Removed" -AutoSize
 	$RemoveAADDevice = Get-AzureADDevice -All:$true | where { ($_.ApproximateLastLogonTimeStamp -le $Days) -and ($_.AccountEnabled -eq $false) } | select-object -ExpandProperty ObjectID
 	
