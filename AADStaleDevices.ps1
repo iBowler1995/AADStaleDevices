eabf6355e2ba00db9c1c4c5c4ea92add0295a2b1
iBowler1995 <93746898+iBowler1995@users.noreply.github.com>
2021-11-15 16:24:51 -0500

    Update AADStaleDevices.ps1

--- AADStaleDevices.ps1
+++ AADStaleDevices.ps1
@@ -3,7 +3,7 @@
 	===========================================================================
 	 Script Name: AADStaleDevices
 	 Created on:   	11/4/2021
-	 Created by:   	Levi Masse
+	 Created by:   	iBowler1995
 	 Filename: AADStaleDevices.ps1
 	===========================================================================
 	.DESCRIPTION
