4d5b441f4f67b455d98ad8b64ad029b95a2da233
iBowler1995 <93746898+iBowler1995@users.noreply.github.com>
2022-01-27 12:15:34 -0500

    Update AADStaleDevices.ps1

--- AADStaleDevices.ps1
+++ AADStaleDevices.ps1
@@ -2,11 +2,11 @@
 	NOTES
 	===========================================================================
 	 Script Name: 	AADStaleDevices
-	 Version:		1.2
+	 Version:	1.2
 	 Created on:   	11/4/2021
 	 Updated on: 	1/27/2022
 	 Created by:   	iBowler1995
-	 Filename: 		AADStaleDevices.ps1
+	 Filename: 	AADStaleDevices.ps1
 	===========================================================================
 	.DESCRIPTION
 	This script is designed to manage stale AzureAD Devices.
@@ -73,12 +73,85 @@ $SMTPPort = "587"
 $EPWD = Get-Content ".\ECred.txt" | ConvertTo-SecureString
 $ECred = New-Object -TypeName System.Management.Automation.PSCredential($From,$EPWD)
 
+#################################################
+
+function Connect-AzureGraph {
+
+    [cmdletbinding()]
+    param(
+    [Parameter()]
+    [String]$AppID,
+    [Parameter()]
+    [String]$TenantID,
+    [Parameter()]
+    [Switch]$Cert,
+    [Parameter()]
+    [Switch]$ClientSecret,
+    [Parameter()]
+    [String]$Thumbprint,
+    [Parameter()]
+    [String]$Secret
+    )
+
+    <#
+		IMPORTANT:
+        ===========================================================================
+        This script is provided 'as is' without any warranty. Any issues stemming 
+        from use is on the user.
+        ===========================================================================
+		.DESCRIPTION
+		Authenticates with MS Graph
+		===========================================================================
+		.PARAMETER AppID
+		AppID of the registered Azure application (if using one)
+		.PARAMETER TenantID
+		Id of your Azure tenant, found here: https://portal.azure.com/#blade/Microsoft_AAD_IAM/ActiveDirectoryMenuBlade/Properties
+        .PARAMETER Cert
+        Switch to use certificate-based authentication
+        .PARAMETER ClientSecret
+        Switch to use Client/Secret-based authentication
+        .PARAMETER SECRET
+        Your client secret
+	#>
+
+    #Ensuring the needed modules are installed
+    if (-Not(Get-Module -ListAvailable -Name "Microsoft.Graph.Authentication")) {
+
+	Install-Module -Name "Microsoft.Graph.Authentication" -Repository PSGallery -Force -AllowClobber
+
+    }
+    if (-Not(Get-Module -ListAvailable -Name "MSAL.PS")) {
+
+	Install-Module -Name "MSAL.PS" -Repository PSGallery -Force -AllowClobber
+
+    }
+
+    If ($Cert) {
+
+        Write-Host "Connecting to MS Graph. . ." -f Yellow
+        Connect-MgGraph -ClientID $AppID -TenantID $TenantID -CertificateThumbprint $Thumbprint
+
+    }
+    elseif ($ClientSecret) {
+
+        Write-Host "Connecting to MS Graph. . ." -f Yellow
+        $MsalToken = Get-MsalToken -TenantId $TenantId -ClientId $AppId -ClientSecret ($ClientSecret | ConvertTo-SecureString -AsPlainText -Force)
+        Connect-Graph -AccessToken $MsalToken.AccessToken
+
+    }
+    else {
+
+        Connect-MgGraph
+
+    }
+
+}
+
+#################################################
+
 #Connects to MS Graph
 Write-Host "Connecting to MS Graph. . ." -f Yellow
-$AppID = ''
-$TenantID = ''
-$Certificate = ''
-Connect-MgGraph -ClientID $AppID -TenantID $TenantID -CertificateThumbprint $Certificate
+Connect-AzureGraph -AppID '' -TenantID '' -Cert -Thumbprint ''
 
 #################################################
 
